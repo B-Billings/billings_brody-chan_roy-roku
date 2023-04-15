8dcb0c48784c097e4fc180d4346ad936a876dbbe
B-Billings <b_billings@fanshaweonline.ca@users.noreply.github.com>
2023-04-15 16:55:15 -0400

    added new database

--- roku_frontend/database/db_rokuusers.sql
+++ roku_frontend/database/db_rokuusers.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 07, 2023 at 10:30 PM
--- Server version: 5.7.36
--- PHP Version: 7.4.26
+-- Host: localhost:3306
+-- Generation Time: Apr 15, 2023 at 08:54 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,17 +27,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_users`
 --
 
-DROP TABLE IF EXISTS `tbl_users`;
-CREATE TABLE IF NOT EXISTS `tbl_users` (
-  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_users` (
+  `id` tinyint(4) NOT NULL,
   `fname` varchar(10) NOT NULL,
   `lname` varchar(10) NOT NULL,
   `password` varchar(10) NOT NULL,
   `avatar` varchar(20) DEFAULT NULL,
   `permissions` tinyint(5) NOT NULL,
-  `username` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `username` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_users`
@@ -45,8 +43,29 @@ CREATE TABLE IF NOT EXISTS `tbl_users` (
 
 INSERT INTO `tbl_users` (`id`, `fname`, `lname`, `password`, `avatar`, `permissions`, `username`) VALUES
 (1, 'Brody', 'Billings', '1234567890', 'brody.jpg', 5, 'BrodyB'),
-(2, 'Roy', 'Chan', '0987654321', 'Roy.jpg', 1, 'RoyC'),
-(3, 'gio', 'p', '1234567890', 'gio.jpg', 3, 'giop');
+(2, 'Roy', 'Chan', '0987654321', 'roy.jpg', 1, 'RoyC'),
+(33, 'Test', 'Images', '1', 'seinfeld.jpg', 5, 'adult-test'),
+(36, 'Kids', 'check', '1', 'count_olaf.jpg', 1, 'Kids-test');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
